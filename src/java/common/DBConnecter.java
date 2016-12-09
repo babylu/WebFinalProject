9991de0b3904b357337cab23b2dceb3f6221c267
babylu <babylu910815@hotmail.com>
2016-12-08 23:37:23 -0500

    new project

--- src/java/common/DBConnecter.java
+++ src/java/common/DBConnecter.java
@@ -7,29 +7,22 @@ package common;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 /**
  *
  * @author babylu
  */
 public class DBConnecter {
-    private ResultSet rs = null;
-    private Connection conn = null;
-    
-    
     public Connection connetDatabase(){
+        Connection conn = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
             String connURL = "jdbc:mysql://localhost:8889/web_final";
             conn = DriverManager.getConnection(connURL, "root", "root");
         } catch (ClassNotFoundException cnfe){
             cnfe.printStackTrace();
-        }
-        catch (SQLException se)
-        {
+        } catch (SQLException se){
             se.printStackTrace();  
         }
         return conn;
